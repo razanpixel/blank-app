85e02ca541084dfcf0aeb8dd2afb6392b96080a3
razanpixel <nigukirazan@gmail.com>
2025-04-23 14:59:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈warzo27mamprang")
+st.write("🎈warzo27mamprang")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "owjssidhdudisissjsisjs."
 )
